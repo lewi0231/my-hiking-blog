0b1675f34c943860f5204edd8386f0f1fb989ee9
Paul Lewis <lewi0231@flinders.edu.au>
2024-05-20 13:36:46 +0930

    styling changes

--- components/auth/auth-modal.tsx
+++ components/auth/auth-modal.tsx
@@ -1,5 +1,4 @@
 import { Button } from "../ui/button";
-import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 import { Dialog, DialogContent, DialogTrigger } from "../ui/dialog";
 import { Social } from "./social";
 
@@ -9,15 +8,8 @@ const AuthModal = () => {
       <DialogTrigger asChild>
         <Button>Log in to comment</Button>
       </DialogTrigger>
-      <DialogContent>
-        <Card className="w-full font-karla">
-          <CardContent className="w-2/3 m-auto">
-            <CardHeader>
-              <CardTitle className=" text-2xl pb-2">Login to Comment</CardTitle>
-            </CardHeader>
-            <Social />
-          </CardContent>
-        </Card>
+      <DialogContent className="w-5/6 rounded-lg">
+        <Social />
       </DialogContent>
     </Dialog>
   );
--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -11,26 +11,26 @@ type Props = {
 
 export const Social = ({ slug }: Props) => {
   return (
-    <div className="flex flex-col justify-center items-center font-karla m-auto gap-2">
+    <div className="flex flex-col justify-center items-center font-karla m-auto gap-2 mt-2 w-full">
       <Button
-        className="w-full border-2 border-opacity-15 shadow-md rounded-xl border-black flex items-center justify-center gap-10"
+        className="w-full border-[1px] border-opacity-15 shadow-sm rounded-xl border-black flex items-center justify-center gap-2 flex-grow"
         variant="outline"
         size="lg"
         onClick={async () => login("google")}
       >
+        <span>Sign in with</span>
         <FcGoogle className="w-5 h-5" />
-        <span>Sign in with Google</span>
       </Button>
 
       <Button
-        className="w-full border-2 border-opacity-15 shadow-md rounded-xl border-black flex items-center justify-center gap-10"
+        className="w-full border-[1px] border-opacity-15 shadow-sm rounded-xl border-black flex items-center justify-center gap-2"
         variant="outline"
         size="lg"
         type="submit"
         onClick={() => login("github")}
       >
+        <span>Sign in with</span>
         <FaGithub className="w-5 h-5" />
-        <span>Sign in with Github</span>
       </Button>
     </div>
   );
